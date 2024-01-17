495730233ddba2554986378f83bb2acafc722c7e
evariste <gwanulagabob9@gmail.com>
2024-01-17 15:24:14 +0100

    feat(threads) added lock implementation in threads

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::collections::HashMap;
 use tokio;
 use std::fs;
 use std::io::{self, Read, BufRead};
+use std::time::{Instant, Duration};
+
 
 struct SharedData {
     request_count: usize,
@@ -21,10 +23,14 @@ async fn send_request(client: &Client, method: Method, url: &str, headers: Heade
         .expect("Failed to send request")
 }
 
-fn thread_function(stop_flag: Arc<AtomicBool>, shared_data: Arc<Mutex<SharedData>>, client: Client, method: Method, url: String, headers: HeaderMap, body: String) {
+fn thread_function(stop_flag: Arc<AtomicBool>, shared_data: Arc<Mutex<SharedData>>, client: Client, method: Method, url: String, headers: HeaderMap, body: String, num_requests: usize) {
     let runtime = tokio::runtime::Runtime::new().unwrap();
     runtime.block_on(async {
-        while !stop_flag.load(Ordering::SeqCst) {
+        for _ in 0..num_requests {
+            if stop_flag.load(Ordering::SeqCst) {
+                break;
+            }
+
             let response = send_request(&client, method.clone(), &url, headers.clone(), &body).await;
             
             let mut data = shared_data.lock().unwrap();
@@ -34,8 +40,41 @@ fn thread_function(stop_flag: Arc<AtomicBool>, shared_data: Arc<Mutex<SharedData
     });
 }
 
+fn display_speed(shared_data: Arc<Mutex<SharedData>>, start: Instant, stop_flag: Arc<AtomicBool>) {
+    loop {
+        thread::sleep(Duration::from_secs(1)); // Update speed every second
+        if stop_flag.load(Ordering::SeqCst) {
+            break;
+        }
+
+        let elapsed_secs = start.elapsed().as_secs_f64();
+        let total_requests = shared_data.lock().unwrap().request_count;
+
+        if elapsed_secs > 0.0 {
+            let requests_per_second = total_requests as f64 / elapsed_secs;
+            println!("Requests: {}, Time: {:.2} seconds, Speed: {:.2} requests/second", total_requests, elapsed_secs, requests_per_second);
+        }
+    }
+}
+/*le code c'est la doc lis et comprend*/
+
 #[tokio::main]
 async fn main() {
+    let banner = r#"
+     
+______                    _     _______                                           _                  
+|_   __ \                  / |_  |_   __ \                                         / |_                
+  | |__) |  __   _   .--. `| |-'   | |__) |  .---.   .--. _  __   _   .---.  .--. `| |-'.---.  _ .--.  
+  |  __ /  [  | | | ( (`\] | |     |  __ /  / /__\\/ /'`\' ][  | | | / /__\\( (`\] | | / /__\\[ `/'`\] 
+ _| |  \ \_ | \_/ |, `'.'. | |,   _| |  \ \_| \__.,| \__/ |  | \_/ |,| \__., `'.'. | |,| \__., | |     
+|____| |___|'.__.'_/[\__) )\__/  |____| |___|'.__.' \__.; |  '.__.'_/ '.__.'[\__) )\__/ '.__.'[___]    
+                                                        |__]                                           
+
+by Bl4ckarch
+@https://github.com/bl4ckarch
+
+    "#; 
+    println!("{}", banner);
     let matches = App::new("HTTP Requester")
         .arg(Arg::with_name("input")
             .short('i')
@@ -50,21 +89,30 @@ async fn main() {
             .value_name("THREADS")
             .help("Number of threads")
             .takes_value(true)
-            .default_value("4"))
+            .default_value("25"))
+        .arg(Arg::with_name("requests")
+            .short('r')
+            .long("requests")
+            .value_name("REQUESTS")
+            .help("Number of requests to make")
+            .takes_value(true)
+            .required(true))
         .get_matches();
-
+    
+    let num_requests: usize = matches.value_of("requests").unwrap().parse().expect("Invalid number for requests");
     let input_path = matches.value_of("input").unwrap();
     let num_threads: usize = matches.value_of("threads").unwrap().parse().expect("Invalid number for threads");
-
-    // Read and parse the input file (this is a simplified example, replace with actual parsing logic)
+    let requests_per_thread = num_requests / num_threads;
+    let mut remaining_requests = num_requests % num_threads;
+    
     let mut file = fs::File::open(input_path).expect("Unable to open file");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Unable to read file");
 
-    let url = "https://www.codeur.com/-seo-referencement-optimisation-trafic-hanoot"; // Replace with actual URL
-    let method = Method::GET; // Replace with actual method
-    let headers = HeaderMap::new(); // Replace with actual headers
-    let body = ""; // Replace with actual body
+    let url = "http://int-proxmoxmonitor.eisge.com/ws/hyperviseur/getVm?node=eh012";
+    let method = Method::GET; 
+    let headers = HeaderMap::new();
+    let body = ""; 
 
     let shared_data = Arc::new(Mutex::new(SharedData {
         request_count: 0,
@@ -74,6 +122,12 @@ async fn main() {
     let stop_flag = Arc::new(AtomicBool::new(false));
 
     let client = Client::new();
+    let start = Instant::now();
+    let display_stop_flag = Arc::clone(&stop_flag);
+    let display_shared_data = Arc::clone(&shared_data);
+    let display_handle = thread::spawn(move || {
+        display_speed(display_shared_data, start, display_stop_flag);
+    });
 
     let mut handles = vec![];
     for _ in 0..num_threads {
@@ -84,9 +138,16 @@ async fn main() {
         let url_clone = url.to_string();
         let headers_clone = headers.clone();
         let body_clone = body.to_string();
+         
+        let thread_requests = if remaining_requests > 0 {
+            remaining_requests -= 1;
+            requests_per_thread + 1
+        } else {
+            requests_per_thread
+        };
 
         let handle = thread::spawn(move || {
-            thread_function(stop_flag_clone, shared_data_clone, client_clone, method_clone, url_clone, headers_clone, body_clone);
+            thread_function(stop_flag_clone, shared_data_clone, client_clone, method_clone, url_clone, headers_clone, body_clone,thread_requests);
         });
         handles.push(handle);
     }
