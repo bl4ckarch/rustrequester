dbb3bee6482450885d781b27f8e5c735b8eaf312
evariste <gwanulagabob9@gmail.com>
2024-01-18 14:45:18 +0100

    feat:added url parse as argument to the execution

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use clap::{App, Arg};
 use reqwest::{Client, Method, header::{HeaderMap}};
-use std::{sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}}, thread};
+use std::{sync::{Arc, Mutex, atomic::{AtomicUsize, AtomicBool, Ordering}}, thread};
 use std::collections::HashMap;
-
 use tokio;
 use std::fs;
 use std::io::{self, Read, BufRead};
 use std::time::{Instant, Duration};
 
-
 struct SharedData {
-    request_count: usize,
-    response_codes: HashMap<u16, usize>,
+    request_count: AtomicUsize,
+    response_codes: Mutex<HashMap<u16, usize>>,
 }
 
 async fn send_request(client: &Client, method: Method, url: &str, headers: HeaderMap, body: &str) -> reqwest::Response {
@@ -23,7 +21,16 @@ async fn send_request(client: &Client, method: Method, url: &str, headers: Heade
         .expect("Failed to send request")
 }
 
-fn thread_function(stop_flag: Arc<AtomicBool>, shared_data: Arc<Mutex<SharedData>>, client: Client, method: Method, url: String, headers: HeaderMap, body: String, num_requests: usize) {
+fn thread_function(
+    stop_flag: Arc<AtomicBool>,
+    shared_data: Arc<SharedData>,
+    client: Client,
+    method: Method,
+    url: String,
+    headers: HeaderMap,
+    body: String,
+    num_requests: u128,
+) {
     let runtime = tokio::runtime::Runtime::new().unwrap();
     runtime.block_on(async {
         for _ in 0..num_requests {
@@ -33,14 +40,14 @@ fn thread_function(stop_flag: Arc<AtomicBool>, shared_data: Arc<Mutex<SharedData
 
             let response = send_request(&client, method.clone(), &url, headers.clone(), &body).await;
             
-            let mut data = shared_data.lock().unwrap();
-            data.request_count += 1;
-            *data.response_codes.entry(response.status().as_u16()).or_insert(0) += 1;
+            shared_data.request_count.fetch_add(1, Ordering::SeqCst);
+            let mut response_codes = shared_data.response_codes.lock().unwrap();
+            *response_codes.entry(response.status().as_u16()).or_insert(0) += 1;
         }
     });
 }
 
-fn display_speed(shared_data: Arc<Mutex<SharedData>>, start: Instant, stop_flag: Arc<AtomicBool>) {
+fn display_speed(shared_data: Arc<SharedData>, start: Instant, stop_flag: Arc<AtomicBool>) {
     loop {
         thread::sleep(Duration::from_secs(1)); // Update speed every second
         if stop_flag.load(Ordering::SeqCst) {
@@ -48,7 +55,7 @@ fn display_speed(shared_data: Arc<Mutex<SharedData>>, start: Instant, stop_flag:
         }
 
         let elapsed_secs = start.elapsed().as_secs_f64();
-        let total_requests = shared_data.lock().unwrap().request_count;
+        let total_requests = shared_data.request_count.load(Ordering::SeqCst);
 
         if elapsed_secs > 0.0 {
             let requests_per_second = total_requests as f64 / elapsed_secs;
@@ -56,23 +63,11 @@ fn display_speed(shared_data: Arc<Mutex<SharedData>>, start: Instant, stop_flag:
         }
     }
 }
-/*le code c'est la doc lis et comprend*/
 
 #[tokio::main]
 async fn main() {
     let banner = r#"
-     
-______                    _     _______                                           _                  
-|_   __ \                  / |_  |_   __ \                                         / |_                
-  | |__) |  __   _   .--. `| |-'   | |__) |  .---.   .--. _  __   _   .---.  .--. `| |-'.---.  _ .--.  
-  |  __ /  [  | | | ( (`\] | |     |  __ /  / /__\\/ /'`\' ][  | | | / /__\\( (`\] | | / /__\\[ `/'`\] 
- _| |  \ \_ | \_/ |, `'.'. | |,   _| |  \ \_| \__.,| \__/ |  | \_/ |,| \__., `'.'. | |,| \__., | |     
-|____| |___|'.__.'_/[\__) )\__/  |____| |___|'.__.' \__.; |  '.__.'_/ '.__.'[\__) )\__/ '.__.'[___]    
-                                                        |__]                                           
-
-by Bl4ckarch
-@https://github.com/bl4ckarch
-
+Your ASCII Art Banner Here
     "#; 
     println!("{}", banner);
     let matches = App::new("HTTP Requester")
@@ -97,27 +92,35 @@ by Bl4ckarch
             .help("Number of requests to make")
             .takes_value(true)
             .required(true))
+        .arg(Arg::with_name("url")
+            .short('u')
+            .long("url")
+            .value_name("URL_TO_TEST")
+            .help("URL to test")
+            .takes_value(true)
+            .required(true))
         .get_matches();
-    
-    let num_requests: usize = matches.value_of("requests").unwrap().parse().expect("Invalid number for requests");
+    let url = matches.value_of("url").unwrap();
+    let num_requests: u128 = matches.value_of("requests").unwrap().parse().expect("Invalid number for requests");
     let input_path = matches.value_of("input").unwrap();
-    let num_threads: usize = matches.value_of("threads").unwrap().parse().expect("Invalid number for threads");
-    let requests_per_thread = num_requests / num_threads;
+    let num_threads: u128 = matches.value_of("threads").unwrap().parse().expect("Invalid number for threads");
+    let requests_per_thread: u128 = num_requests / num_threads;
     let mut remaining_requests = num_requests % num_threads;
     
     let mut file = fs::File::open(input_path).expect("Unable to open file");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Unable to read file");
 
-    let url = "http://int-proxmoxmonitor.eisge.com/ws/hyperviseur/getVm?node=eh012";
-    let method = Method::GET; 
-    let headers = HeaderMap::new();
-    let body = ""; 
+    // Here you should parse the contents variable to extract the actual URL, method, headers, and body.
+    let url = url.to_string();  // Placeholder
+    let method = Method::GET;  // Placeholder
+    let headers = HeaderMap::new();  // Placeholder
+    let body = "";  // Placeholder
 
-    let shared_data = Arc::new(Mutex::new(SharedData {
-        request_count: 0,
-        response_codes: HashMap::new(),
-    }));
+    let shared_data = Arc::new(SharedData {
+        request_count: AtomicUsize::new(0),
+        response_codes: Mutex::new(HashMap::new()),
+    });
 
     let stop_flag = Arc::new(AtomicBool::new(false));
 
@@ -125,7 +128,7 @@ by Bl4ckarch
     let start = Instant::now();
     let display_stop_flag = Arc::clone(&stop_flag);
     let display_shared_data = Arc::clone(&shared_data);
-    let _display_handle = thread::spawn(move || {
+    let display_handle = thread::spawn(move || {
         display_speed(display_shared_data, start, display_stop_flag);
     });
 
@@ -138,7 +141,7 @@ by Bl4ckarch
         let url_clone = url.to_string();
         let headers_clone = headers.clone();
         let body_clone = body.to_string();
-         
+        
         let thread_requests = if remaining_requests > 0 {
             remaining_requests -= 1;
             requests_per_thread + 1
@@ -147,7 +150,7 @@ by Bl4ckarch
         };
 
         let handle = thread::spawn(move || {
-            thread_function(stop_flag_clone, shared_data_clone, client_clone, method_clone, url_clone, headers_clone, body_clone,thread_requests);
+            thread_function(stop_flag_clone, shared_data_clone, client_clone, method_clone, url_clone, headers_clone, body_clone, thread_requests);
         });
         handles.push(handle);
     }
@@ -167,7 +170,10 @@ by Bl4ckarch
         handle.join().unwrap();
     }
 
-    let data = shared_data.lock().unwrap();
-    println!("Total requests: {}", data.request_count);
-    println!("Response codes: {:?}", data.response_codes);
+    display_handle.join().unwrap(); // Ensure the display thread has finished before final output
+
+    let total_requests = shared_data.request_count.load(Ordering::SeqCst);
+    let response_codes = shared_data.response_codes.lock().unwrap();
+   // println!("Total requests: {}", total_requests);
+    //println!("Response codes: {:?}", *response_codes);
 }
